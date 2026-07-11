ea4fa8dd10fa1d3bc422ed619d085bbf361ff794
yusufrokib123-rgb <yusufrokib123@gmail.com>
2026-07-11 15:56:06 +0100

    Create matches.js

---
+++ routes/matches.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const axios = require("axios");
+
+const router = express.Router();
+
+router.get("/matches", async (req, res) => {
+    try {
+        const response = await axios.get(
+            "https://api.football-data.org/v4/matches",
+            {
+                headers: {
+                    "X-Auth-Token": process.env.FOOTBALL_API_KEY
+                }
+            }
+        );
+
+        res.json(response.data);
+
+    } catch (error) {
+
+        console.error(error.response?.data || error.message);
+
+        res.status(500).json({
+            success: false,
+            message: "Unable to fetch matches"
+        });
+    }
+});
+
+module.exports = router;
