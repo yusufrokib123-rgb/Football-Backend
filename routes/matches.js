827c2526059f4c1226077bd4810fbed8e57009ca
yusufrokib123-rgb <yusufrokib123@gmail.com>
2026-07-13 09:11:18 +0100

    Update matches.js

--- routes/matches.js
+++ routes/matches.js
@@ -3,6 +3,10 @@ const axios = require("axios");
 
 const router = express.Router();
 
+// ========================
+// ALL MATCHES
+// ========================
+
 router.get("/matches", async (req, res) => {
     try {
         const response = await axios.get(
@@ -24,6 +28,36 @@ router.get("/matches", async (req, res) => {
             success: false,
             message: "Unable to fetch matches"
         });
+    }
+});
+
+// ========================
+// ALL COMPETITIONS
+// ========================
+
+router.get("/competitions", async (req, res) => {
+    try {
+
+        const response = await axios.get(
+            "https://api.football-data.org/v4/competitions",
+            {
+                headers: {
+                    "X-Auth-Token": process.env.FOOTBALL_API_KEY
+                }
+            }
+        );
+
+        res.json(response.data);
+
+    } catch (error) {
+
+        console.error(error.response?.data || error.message);
+
+        res.status(500).json({
+            success: false,
+            message: "Unable to fetch competitions"
+        });
+
     }
 });
 
